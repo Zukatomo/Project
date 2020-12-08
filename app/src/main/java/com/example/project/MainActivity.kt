a076d654f3a785d8a6f1e134d312328dbcc46074
zukatomo <zukatomo0@gmail.com>
2020-12-08 22:24:40 +0200

    1

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,22 +2,31 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // inflate layout
 
-        //  set up  BottomNavigation
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomnav)
-        bottomNav.setupWithNavController(navController)
+        mNavController = (supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment).navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+
+        bottomNav.setupWithNavController(mNavController)
+
     }
 }
--- app/src/main/java/com/example/project/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.project
-
-data class User(var name: String = "", var address: String = "", var phone: String = "", var email: String = "")
