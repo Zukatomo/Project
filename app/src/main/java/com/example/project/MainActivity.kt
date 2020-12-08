0a405f08c858091c7914da2440d0e05661ac70eb
zukatomo <zukatomo0@gmail.com>
2020-12-08 09:38:04 +0200

    dsaasd

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //  set up  BottomNavigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomnav)
+        bottomNav.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/project/User.kt
@@ -0,0 +1,3 @@
+package com.example.project
+
+data class User(var name: String = "", var address: String = "", var phone: String = "", var email: String = "")
